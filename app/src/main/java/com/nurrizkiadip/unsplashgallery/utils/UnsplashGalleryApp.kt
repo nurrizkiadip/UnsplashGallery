2d938117ae7d8cdcb6097e99a4ac2beabb5006cd
Nur Rizki Adi Prasetyo <18102064@ittelkom-pwt.ac.id>
2022-01-02 17:31:21 +0700

    Delete UnsplashGalleryApp.kt

--- app/src/main/java/com/nurrizkiadip/unsplashgallery/utils/UnsplashGalleryApp.kt
+++
@@ -1,7 +0,0 @@
-package com.nurrizkiadip.unsplashgallery.utils
-
-import android.app.Application
-
-open class UnsplashGalleryApp : Application() {
-    open fun getBaseUrl() = "https://api.unsplash.com/"
-}
